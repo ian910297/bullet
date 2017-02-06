08e874d8e48d5e69d449c6df9a0c42f50b73f304
demonic <ian910297@gmail.com>
2017-02-06 09:15:49 +0800

    add boot animation and welcome text, and a click event

--- states/boot.js
+++ states/boot.js
@@ -6,15 +6,35 @@ Boot.prototype = {
     game.load.script('utils', '/lib/utils.js');
     game.load.script('boot-animation', '/lib/boot-animation.js');
     game.load.script('load', '/states/load.js');
+
     console.log('Boot');
   },
 
-  create () {
+  async create () {
     var anim = new BootAnimation(800, 600, gameOptions.tileW, gameOptions.tileH);
-    anim.start();
+    await anim.start('random');
+    this.welcomeText();
 
     game.state.add('Load', Load);
-//    game.state.start('Load');
+    game.input.onDown.addOnce(this.touchToContinue, this);
   },
+
+  welcomeText () {
+    var text = game.add.text(0, game.world.centerY, '', {font: '48px', fill: '#000000', align: 'center'});
+    text.anchor.setTo(0, 0);
+    var shineFlag = 0;
+    setInterval(() => {
+      if(shineFlag%2 == 0) {
+        text.setText('Touch to continue');
+      } else {
+        text.setText('Touch to continue ...');
+      }
+      shineFlag += 1;
+    }, 500);
+  },
+
+  touchToContinue () {
+    game.state.start('Load');
+  }
 };
 
